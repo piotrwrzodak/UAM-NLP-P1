f9ce8e6a3cd7cdc31f6192c9df30209a221624a7
piotrwrzodak <piotrwrzodakk@gmail.com>
2022-01-08 20:24:13 +0100

    get dataset

---
+++ getData.py
@@ -0,0 +1,11 @@
+import json
+import requests
+
+
+def get_data():
+    resp = requests.get(url="https://justjoin.it/api/offers")
+    data = resp.json()
+
+    with open('data.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
---
+++ main.py
@@ -0,0 +1,4 @@
+from getData import get_data
+
+if __name__ == "__main__":
+    get_data()
